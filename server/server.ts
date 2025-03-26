57d3f89d1f87fd35b2c1b60339a4900f49dc9f04
sebastianmdahl <sebastian.moen.dahl@hotmail.com>
2025-03-26 20:28:54 +0100

    hono serves ouput from Vite as static files

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+
 const app = new Hono();
 app.get("/api/hello", async (c) => {
   return c.text("Hello World!");
 });
+app.use("*", serveStatic({ root: "../dist" }));
 serve(app);
