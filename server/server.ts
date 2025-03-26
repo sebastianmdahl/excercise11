2e1ebc31dc698f52b92e6b9158ffc445cd144a42
sebastianmdahl <sebastian.moen.dahl@hotmail.com>
2025-03-26 21:15:28 +0100

    start script and postinstall script for heroku

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,19 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
+import * as process from "node:process";
 
 const app = new Hono();
+
 app.get("/api/hello", async (c) => {
-  return c.text("Hello World!");
+  return c.text("Hello Hono!");
 });
+
+// Serve static files from the frontend build
 app.use("*", serveStatic({ root: "../dist" }));
-serve(app);
+
+// Start the server on the correct port
+serve({
+  fetch: app.fetch,
+  port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+});
